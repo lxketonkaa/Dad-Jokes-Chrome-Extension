1322222c7db6a808d800c4bb0d662b0e41a7b804
lxketonkaa <99335393+lxketonkaa@users.noreply.github.com>
2022-03-24 20:51:43 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-fetch('https://icanhazdadjoke.com/slack')
-    .then(data => data.json())
-    .then(jokeData => {
-        const jokeText = jokeData.attachments[0].text;
-        const jokeElement = document.getElementById('jokeElement');
+var axios = require("axios").default;
 
-        jokeElement.innerHTML = jokeText;
-    })
+var options = {
+  method: 'GET',
+  url: 'https://healthruwords.p.rapidapi.com/v1/quotes/',
+  params: {t: 'Wisdom', maxR: '1', size: 'medium', id: '731'},
+  headers: {
+    'X-RapidAPI-Host': 'healthruwords.p.rapidapi.com',
+    'X-RapidAPI-Key': '86aae83eb5msh17535b44750474cp1133a2jsn2f7754f85517'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
